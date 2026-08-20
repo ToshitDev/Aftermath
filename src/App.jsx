a9669d967c3bda0c6347e523fc8e167bd1077b11
Toshit <toshitrepala24@gmail.com>
2026-08-20 21:30:33 +0530

    chore: scaffold Vite + React + Tailwind project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-neutral-950 text-neutral-100">
+      <h1 className="text-4xl font-bold">Aftermath</h1>
+    </div>
+  )
+}
+
+export default App
